ad9e135afdc8642591c7bd3171fa6febb7851e45
bound1ess <bound1ess.dev@gmail.com>
2016-02-13 21:59:22 +0600

    Working on something interesting.

--- 622/a.cpp
+++ 622/a.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <cstdio>
 
 using namespace std;
 
 int main() {
+  freopen("input", "rt", stdin);
+  freopen("output", "wt", stdout);
   long long n;
   cin >> n;
   --n;
