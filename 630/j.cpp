e3c8644612a19195ab2668c0c9846339325a097c
bound1ess <bound1ess.dev@gmail.com>
2016-02-22 17:40:35 +0600

    Add 630j.

---
+++ 630/j.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  freopen("input", "rt", stdin);
+  freopen("output", "wt", stdout);
+  long long n;
+  cin >> n;
+  cout << (n / 2520LL) << endl;
+  return 0;
+}
