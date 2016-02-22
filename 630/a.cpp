473008ab217eb589d8ce1e3fb20b0516ca5b2a00
bound1ess <bound1ess.dev@gmail.com>
2016-02-22 16:12:03 +0600

    Add 630a.

---
+++ 630/a.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  freopen("input", "rt", stdin);
+  freopen("output", "wt", stdout);
+  long long n;
+  cin >> n;
+  cout << 25 << endl;
+  return 0;
+}
